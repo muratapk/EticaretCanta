6bed478751b4d1810488b7baf242932f02a0107f
muratapk <muratapk@hotmail.com>
2025-08-23 13:57:17 +0300

    enson kısım

---
+++ EticaretCanta/Component/CategoryList.cs
@@ -0,0 +1,20 @@
+﻿using EticaretCanta.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EticaretCanta.Component
+{
+    public class CategoryList:ViewComponent
+    {
+        private readonly Baglanti _context;
+
+        public CategoryList(Baglanti context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }   
+    }
+}
