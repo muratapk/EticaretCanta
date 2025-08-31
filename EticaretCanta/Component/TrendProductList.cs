7e752cf5d67fc9ac595fd0c2bc6084f3f0ccf3de
muratapk <muratapk@hotmail.com>
2025-08-31 11:25:07 +0300

    ürün Sayfası Ekleme yapıldı

---
+++ EticaretCanta/Component/TrendProductList.cs
@@ -0,0 +1,21 @@
+﻿using EticaretCanta.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EticaretCanta.Component
+{
+    public class TrendProductList:ViewComponent
+    {
+        private readonly Baglanti _context;
+
+        public TrendProductList(Baglanti context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var trendProducts = _context.Products.OrderByDescending(p=>p.Product_Id).Take(10).ToList();
+            return View(trendProducts);
+        }
+
+    }
+}
--- EticaretCanta/Controllers/HomeController.cs
+++ EticaretCanta/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using EticaretCanta.Data;
 using EticaretCanta.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,10 +8,11 @@ namespace EticaretCanta.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly Baglanti _context;
+        public HomeController(ILogger<HomeController> logger,Baglanti context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -21,6 +23,11 @@ namespace EticaretCanta.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+        public async Task<IActionResult> Product_Details(int ? id)
+        {
+            var product_List=_context.Products.FirstOrDefault(p => p.Product_Id == id);
+            return View(product_List);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
