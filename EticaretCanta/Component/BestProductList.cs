ab50abca6e9ea59d0331f95220f661b01f3ecec5
muratapk <muratapk@hotmail.com>
2025-09-06 13:35:02 +0300

    Ürün detay Sayfası

---
+++ EticaretCanta/Component/BestProductList.cs
@@ -0,0 +1,21 @@
+﻿using EticaretCanta.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EticaretCanta.Component
+{
+    public class BestProductList:ViewComponent
+    {
+        private readonly Baglanti _context;
+
+        public BestProductList(Baglanti context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var liste = _context.Products.Include(c=>c.Category).Include(t=>t.Sub_Category).Include(s=>s.Pictures).OrderByDescending(p => p.Product_Id).Take(8).ToList();
+            return View(liste);
+        }
+    }
+}
